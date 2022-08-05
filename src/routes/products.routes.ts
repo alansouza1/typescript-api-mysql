bd31e4d3e91af08baaa9250774bc289e562c6e5c
alansouza1 <alansouza6000@gmail.com>
2022-08-05 18:29:29 -0300

    Requisito 2 finalizado

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 const productsController = new ProductsController();
 
+router.get('/products', productsController.getAll);
 router.post('/products', productsController.create);
 
 export default router;
